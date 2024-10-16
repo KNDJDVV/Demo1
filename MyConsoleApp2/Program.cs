06aa871ba3a425b941a40b873eccc8852a72372b
Juan Valencia <vjuandavid03@gmail.com>
2024-10-16 17:38:58 -0500

    Incluir la libreria en MyconsoleApp ejemplo en program.cs

--- MyConsoleApp2/Program.cs
+++ MyConsoleApp2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mylibrary;
+namespace MyConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Writer("Hello, Writer!");
+            MessageWriter FWriter = new MessageWriter();
+            FWriter.Writer("Hello, Figgle!!!");
+        }
+    }
+}
